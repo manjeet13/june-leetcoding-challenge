7c29648703acf7cb6794e4da01bfd4e13d5498d6
Manjeet <manjeet@enterprisebot.org>
2020-06-01 19:16:55 +0530

    week 1, day 1

---
+++ src/week1/invert_binary_tree.js
@@ -0,0 +1,45 @@
+/* 
+PROBLEM: Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+ */
+
+//  SOLUTION
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(!root) return root;
+    // if(root.length == 0) return [];
+    invertTree(root.left);
+    invertTree(root.right);
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    return root;
+};
