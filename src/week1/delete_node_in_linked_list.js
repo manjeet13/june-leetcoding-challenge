621aced1f6a549d6914253048fa158014540ba26
Manjeet <manjeet@enterprisebot.org>
2020-06-02 15:09:15 +0530

    week 1, day 2

---
+++ src/week1/delete_node_in_linked_list.js
@@ -0,0 +1,25 @@
+// PROBLEM
+/* 
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+ */
+// SOLUTION 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
